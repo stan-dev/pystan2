b1c7edcf562f426c758d5352305297aef7bf1491
Allen Riddell <abr@ariddell.org>
2013-08-25 12:26:24 -0400

    v0.2.0 release

--- doc/whats_new.rst
+++ doc/whats_new.rst
@@ -6,6 +6,14 @@
  What's New 
 ============
 
+v.0.2.0 (25. August 2013)
+=========================
+
+- ``optimizing`` method working for scalar, vector, and matrix parameters
+- stanfit objects now have a ``summary`` and ``__str__`` methods à la RStan
+- stan source updated to commit cc82d51d492d26f754fd56efe22a99191c80217b (July 26, 2013)
+- IPython-relevant bug fixes
+
 v.0.1.1 (19. July 2013)
 =======================
 
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 3',
     'Programming Language :: Cython',
-    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Operating System :: OS Independent',
     'Intended Audience :: Developers',
@@ -50,9 +50,9 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering :: Information Analysis'
 ]
 MAJOR = 0
-MINOR = 1
-MICRO = 2
-ISRELEASED = False
+MINOR = 2
+MICRO = 0
+ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 FULLVERSION = VERSION
