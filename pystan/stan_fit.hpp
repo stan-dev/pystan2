a3deafdfa8233ff2d4b8c6fcb75ecd689d954c0e
Allen Riddell <abr@ariddell.org>
2013-09-23 21:26:25 -0400

    Use 0-indexing in parameter names

--- pystan/stan_fit.hpp
+++ pystan/stan_fit.hpp
@@ -289,7 +289,7 @@ namespace pystan {
       fnames.clear(); 
       for (size_t i = 0; i < names.size(); ++i) {
         std::vector<std::string> i_names; 
-        get_flatnames(names[i], dims[i], i_names, col_major, true); // col_major = true
+        get_flatnames(names[i], dims[i], i_names, col_major, false); // col_major = true, first_is_one = false for PyStan (true for RStan)
         fnames.insert(fnames.end(), i_names.begin(), i_names.end());
       } 
     } 
