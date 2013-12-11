9ff2d46b7e75ba570f622dd46163b459b8032b08
Allen Riddell <abr@ariddell.org>
2013-12-11 08:33:46 -0500

    TST: Test user-provided initial value that is a matrix.

--- pystan/tests/test_user_inits.py
+++ pystan/tests/test_user_inits.py
@@ -1,7 +1,10 @@
 import unittest
 
+import numpy as np
+
 import pystan
 from pystan._compat import PY2
+
 
 class TestUserInits(unittest.TestCase):
 
@@ -61,3 +64,48 @@ class TestUserInits(unittest.TestCase):
         with assertRaisesRegex(RuntimeError, "sigma missing"):
             pystan.stan(model_code=model_code, iter=10, chains=1, seed=2,
                         data=data, init=[dict(mu=4)], warmup=0)
+
+
+class TestUserInitsMatrix(unittest.TestCase):
+
+    model_code = """
+    data {
+    int<lower=2> K;
+    int<lower=1> D;
+    }
+    parameters {
+    matrix[K,D] beta;
+    }
+    model {
+    for (k in 1:K)
+        for (d in 1:D)
+          beta[k,d] ~ normal(if_else(d==2,100, 0),1);
+    }"""
+    model = pystan.StanModel(model_code=model_code)
+    data = dict(K=3, D=4)
+
+    def test_user_init(self):
+        model_code = self.model_code
+        data = self.data
+        beta = np.ones((data['K'], data['D']))
+        fit1 = pystan.stan(model_code=model_code, iter=10, chains=1, seed=2,
+                           data=data, init=[dict(beta=beta)], warmup=0)
+        np.testing.assert_equal(fit1.get_inits()[0]['beta'], beta)
+        beta = 5 * np.ones((data['K'], data['D']))
+        fit2 = pystan.stan(model_code=model_code, iter=10, chains=1, seed=2,
+                           data=data, init=[dict(beta=beta)], warmup=0)
+        np.testing.assert_equal(fit2.get_inits()[0]['beta'], beta)
+
+    def test_user_initfun(self):
+        model_code = self.model_code
+        data = self.data
+
+        beta = np.ones((data['K'], data['D']))
+
+        def make_inits(chain_id):
+            return dict(beta=beta * chain_id)
+
+        fit1 = pystan.stan(model_code=model_code, iter=10, chains=4, seed=2,
+                           data=data, init=make_inits, warmup=0)
+        for i, inits in enumerate(fit1.get_inits()):
+            np.testing.assert_equal(beta * i, inits['beta'])
