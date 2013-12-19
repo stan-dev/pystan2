3d4d99869c22be87307e65ea001d6c7084e590f8
Allen Riddell <abr@ariddell.org>
2013-12-18 22:09:35 -0500

    ENH: Raise ValueError with explanation when stanc fails to parse model code
    Thanks to Nathan Sanders for spotting this.

--- pystan/api.py
+++ pystan/api.py
@@ -120,7 +120,7 @@ def stanc(file=None, charset='utf-8', model_code=None, model_name="anon_model",
 
     result = pystan._api.stanc(model_code_bytes, model_name_bytes)
     if result['status'] == -1:  # EXCEPTION_RC = -1
-        logger.error("Error parsing:\n{}".format(result['msg']))
+        raise ValueError("Error parsing model code:\n{}".format(result['msg']))
     del result['msg']
     result.update({'model_name': model_name})
     result.update({'model_code': model_code})
