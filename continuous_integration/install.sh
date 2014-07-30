acedcfee2501711012837b8ff1399f909ae5bcdd
Allen Riddell <abr@ariddell.org>
2014-07-29 21:35:50 -0400

    TST: Test against OS X and Linux on travis

---
+++ continuous_integration/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variables defined
+# in the .travis.yml in the top level folder of the project.
+
+set -e
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    # fix a crash with multiprocessing on Travis
+    sudo rm -rf /dev/shm
+    sudo ln -s /run/shm /dev/shm
+fi
+
+
+# steps common to linux and OS X
+
+# use Anaconda to get compiled versions of scipy and numpy,
+# modified from https://gist.github.com/dan-blanchard/7045057
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh -O miniconda.sh; fi
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-MacOSX-x86_64.sh -O miniconda.sh; fi
+chmod +x miniconda.sh
+./miniconda.sh -b
+export PATH=$HOME/miniconda/bin:$PATH
+# Update conda itself
+conda update --yes --quiet conda
+if [[ $TRAVIS_PYTHON_VERSION != '3.4' ]]; then conda create --quiet --yes -n env_name python=$TRAVIS_PYTHON_VERSION pip Cython=0.19 numpy=1.7 scipy nose matplotlib; fi
+if [[ $TRAVIS_PYTHON_VERSION == '3.4' ]]; then conda create --quiet --yes -n env_name python=$TRAVIS_PYTHON_VERSION pip Cython numpy scipy nose matplotlib; fi
+source activate env_name
+
+# use pip install . rather than python setup.py install due to travis log limits
+pip install .
---
+++ continuous_integration/test_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variables defined
+# in the .travis.yml in the top level folder of the project.
+
+set -e
+
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+
+# Skip tests that require large downloads over the network to save bandwith
+# usage as travis workers are stateless and therefore traditional local
+# disk caching does not work.
+SKLEARN_SKIP_NETWORK_TESTS=1
+
+# TODO: if Mac OS X graphics are a problem, perhaps make this setting do something
+# look at SKLEARN_SKIP_NETWORK_TESTS in scikit-learn tests
+export PYSTAN_SKIP_PLOT_TESTS=1
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    DISPLAY=:99.0 JOBLIB_MULTIPROCESSING=0 nosetests -w /tmp --process-timeout=1200 --processes=2 --exclude=$TEST_SUITE_REGEX pystan.tests
+fi
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # skip DISPLAY setting and hope for the best
+    JOBLIB_MULTIPROCESSING=0 nosetests -w /tmp --process-timeout=1200 --processes=2 --exclude=$TEST_SUITE_REGEX pystan.tests
+fi
