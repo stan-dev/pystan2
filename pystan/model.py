a2a441c6f10bcbba442b886b7132f9bb41967001
Allen Riddell <abr@ariddell.org>
2014-09-04 21:41:46 -0400

    stanfit4model.pyx needs numpy libraries to compile
    Add ``np.get_include()`` to include_dirs

--- pystan/model.py
+++ pystan/model.py
@@ -254,11 +254,14 @@ class StanModel:
         temp_dir = tempfile.mkdtemp()
         lib_dir = os.path.join(temp_dir, 'pystan')
         pystan_dir = os.path.dirname(__file__)
-        include_dirs = [lib_dir,
-                        pystan_dir,
-                        os.path.join(pystan_dir, "stan/src"),
-                        os.path.join(pystan_dir, "stan/lib/eigen_3.2.0"),
-                        os.path.join(pystan_dir, "stan/lib/boost_1.54.0")]
+        include_dirs = [
+            lib_dir,
+            pystan_dir,
+            os.path.join(pystan_dir, "stan/src"),
+            os.path.join(pystan_dir, "stan/lib/eigen_3.2.0"),
+            os.path.join(pystan_dir, "stan/lib/boost_1.54.0"),
+            np.get_include(),
+        ]
         library_dirs = [os.path.join(pystan_dir)]
         libraries = ['stan']
 
