665fe64546e1ae8f529c555f93916acb9c249ec6
Allen Riddell <abr@ariddell.org>
2015-12-11 08:25:19 +0100

    WIP adjusting model.py vb method

--- pystan/model.py
+++ pystan/model.py
@@ -848,6 +848,7 @@ class StanModel:
         seed = pystan.misc._check_seed(seed)
 
         stan_args = dict(iter=iter,
+                         chain_id=1,
                          seed=seed,
                          method="variational",
                          algorithm=algorithm)
@@ -866,4 +867,36 @@ class StanModel:
         stan_args = pystan.misc._get_valid_stan_args(stan_args)
 
         ret, sample = fit._call_sampler(stan_args)
-        return sample
+        samples = [sample]
+
+        # _organize_inits strips out lp__ (RStan does it in this method)
+        inits_used = pystan.misc._organize_inits([s['inits'] for s in samples], m_pars, p_dims)
+
+        random_state = np.random.RandomState(stan_args['seed'])
+        chains = 1
+        output_samples = int(stan_args['ctrl']['output_samples'])
+        perm_lst = [random_state.permutation(output_samples) for _ in range(chains)]
+        fnames_oi = fit._get_param_fnames_oi()
+        n_flatnames = len(fnames_oi)
+        fit.sim = {'samples': samples,
+                   # rstan has this; name clashes with 'chains' in samples[0]['chains']
+                   'chains': len(samples),
+                   'iter': iter,
+                   'warmup': 0,
+                   'thin': 1,
+                   'n_save': [output_samples] * chains,
+                   'warmup2': [0] * chains,
+                   'permutation': perm_lst,
+                   'pars_oi': fit._get_param_names_oi(),
+                   'dims_oi': fit._get_param_dims_oi(),
+                   'fnames_oi': fnames_oi,
+                   'n_flatnames': n_flatnames}
+        fit.model_name = self.model_name
+        fit.model_pars = m_pars
+        fit.par_dims = p_dims
+        fit.mode = 0 if not kwargs.get('test_grad') else 1
+        fit.inits = inits_used
+        fit.stan_args = [stan_args]
+        fit.stanmodel = self
+        fit.date = datetime.datetime.now()
+        return fit
