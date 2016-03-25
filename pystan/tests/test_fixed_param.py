bfa1b74f4a4bd9474e22cb0be8a5169a9a2271ad
Allen Riddell <abr@ariddell.org>
2016-03-25 12:52:26 -0400

    TST: Add Fixed_param test

---
+++ pystan/tests/test_fixed_param.py
@@ -0,0 +1,29 @@
+import unittest
+
+import numpy as np
+
+import pystan
+from pystan._compat import PY2
+
+
+class TestFixedParam(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        model_code = '''
+            model { }
+            generated quantities {
+              real y;
+              y <- normal_rng(0, 1);
+            }
+        '''
+        cls.model = pystan.StanModel(model_code=model_code)
+
+    def test_fixed_param(self):
+        fit = self.model.sampling(algorithm='Fixed_param')
+        self.assertEqual(fit.sim['iter'], 2000)
+        self.assertEqual(fit.sim['pars_oi'], ['y', 'lp__'])
+        self.assertEqual(len(fit.sim['samples']), 4)
+        for sample_dict in fit.sim['samples']:
+            assert -0.5 < np.mean(sample_dict['chains']['y']) < 0.5
+            assert 0.5 < np.std(sample_dict['chains']['y']) < 1.5
