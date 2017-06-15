f3b0590996ea499e25ddc29b3b009dfe9808db3d
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2017-06-15 22:07:34 +0300

    Create test_multinormal_compilation.py
    This test should test if compilation fail on Windows MSVC

---
+++ pystan/tests/test_multinormal_compilation.py
@@ -0,0 +1,29 @@
+import unittest
+
+import numpy as np
+from pystan import StanModel
+
+
+class TestMultiNormalCompilation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+      
+        model_code = """
+        data {
+          int<lower=0> M;
+          int<lower=0> N;
+          vector[N] y;
+          matrix[N, M] A;
+          matrix[N, N] Sigma;
+        }
+        parameters {
+          vector[M] x;
+        }
+        model {
+          y ~ multi_normal(y2 + A * x, Sigma);
+        }"""
+        cls.model = StanModel(model_code=model_code)
+
+    def test_compilation(self):
+        pass
