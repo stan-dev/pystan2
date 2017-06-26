a872387d1e379d304253104b369a329b676c3451
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2017-06-26 23:26:09 +0300

    Test for fit.summary() with pars argument

--- pystan/tests/test_misc_args.py
+++ pystan/tests/test_misc_args.py
@@ -9,7 +9,7 @@ from pystan._compat import PY2
 class TestArgs(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        model_code = 'parameters {real y;} model {y ~ normal(0,1);}'
+        model_code = 'parameters {real x;real y;real z;} model {x ~ normal(0,1);y ~ normal(0,1);z ~ normal(0,1)}'
         cls.model = pystan.StanModel(model_code=model_code)
 
     def test_control(self):
@@ -23,3 +23,21 @@ class TestArgs(unittest.TestCase):
             model.sampling(control=control_invalid)
         with assertRaisesRegex(ValueError, '`metric` must be one of'):
             model.sampling(control={'metric': 'lorem-ipsum'})
+            
+    def test_print_summary(self):
+        model = self.model
+        fit = model.sampling(iter=100)
+        
+        summary_full = fit.summary()
+        summary_one_par1 = fit.summary(pars='z')
+        summary_one_par2 = fit.summary(pars=['z'])
+        summary_pars = fit.summary(pars=['x', 'y'])
+        
+        assertNotEqual(summary_full, summary_one_par1)
+        assertNotEqual(summary_full, summary_one_par2)
+        assertNotEqual(summary_full, summary_pars)
+        assertNotEqual(summary_one_par1, summary_one_par2)
+        assertNotEqual(summary_one_par1, summary_pars)
+        assertNotEqual(summary_one_par2, summary_pars)
+        
+        assertEqual(summary_one_par1, summary_one_par2)
