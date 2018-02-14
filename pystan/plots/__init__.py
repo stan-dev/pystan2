bbe31341aca5875313b074857151a5c080e4e46e
ahartikainen <ari.hartikainen@aalto.fi>
2018-02-14 08:15:57 +0200

    add pystan.plots to submodules in setup

--- pystan/plots/__init__.py
+++ pystan/plots/__init__.py
@@ -1,2 +1,2 @@
-from .plots_plotting import traceplot, forestplot, parcoords
+from .plotting import traceplot, forestplot, parcoords
 from .plots_data import traceplot_data, forestplot_data, parcoords_data, kde_data, hist_data
