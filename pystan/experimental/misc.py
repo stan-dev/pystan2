7a997507e8b21ec85a2ddcc28616713905a46825
Ari Hartikainen <ari.hartikainen@aalto.fi>
2018-04-28 01:30:31 +0300

    fix docstring to follow numpy style

--- pystan/experimental/misc.py
+++ pystan/experimental/misc.py
@@ -5,24 +5,30 @@ import os
 logger = logging.getLogger('pystan')
 
 def fix_include(model_code):
-    """Function to normalize (remove whitespace) around the #include statements.
+    """Reformat `model_code` (remove whitespace) around the #include statements.
     
     Note
     ----
-    This functions return edited version of the model_code.
+        A modified `model_code` is returned.
 
     Parameters
     ----------
-    model_code, str
+    model_code : str
+        Model code
 
     Returns
     -------
-    new_model_code, str
+    str
+        Reformatted model code
     
     Example
     -------
-    from pystan.experimental import fix_include
-    model_code = fix_include(
+        
+    >>> from pystan.experimental import fix_include
+    >>> model_code = "parameters { #include myfile.stan \n..."
+    >>> model_code_reformatted = fix_include(model_code)
+    # "parameters {#include myfile.stan\n..."
+
     """
     pattern = r"(?<=\n)\s*(#include)\s*(\S+)\s*(?=\n)"
     model_code, n = re.subn(pattern, r"\1 \2", model_code)
