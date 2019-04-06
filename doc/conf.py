09ba41c51a80bec3459604c90e663aa89af9f389
Allen Riddell <riddella+stan@indiana.edu>
2019-04-06 15:32:53 -0400

    fix(doc): remove sphinx.ext.pngmath, unused

--- doc/conf.py
+++ doc/conf.py
@@ -62,7 +62,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
